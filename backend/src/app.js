1fb52caadeb4ae79b4839ea5391de93ce09d5ee9
Ariana-UAlbany <acnieves@albany.edu>
2025-11-21 05:32:50 +0000

    Quotes 1.3 and Bruno Manual Test Creation

--- backend/src/app.js
+++ backend/src/app.js
@@ -83,6 +83,12 @@ let happinessQuotes = [
     'author': 'Ralph Waldo Emerson'
   }
 ];
+const categoryMap = {// categories holds an array of strings, categoryMap holds the array of category arrays
+      successQuotes,
+      perseveranceQuotes,
+      happinessQuotes
+    };
+
 //Quotes 1.1: Create a GET endpoint at /quotebook/categories
 app.get('/quotebook/categories/', (req, res) => {
   let category = "";
@@ -96,12 +102,7 @@ app.get('/quotebook/categories/', (req, res) => {
 //Quotes 1.2: Create a GET endpoint at /quotebook/quote/:category 
 app.get('/quotebook/quote/:category', (req, res) => {
   let category = req.params['category'];
-  if(categories.includes(category)){ 
-    const categoryMap = {// categories holds an array of strings, categoryMap holds the array of category arrays
-      successQuotes,
-      perseveranceQuotes,
-      happinessQuotes
-    };
+  if(categories.includes(category)){
     const list = categoryMap[category];//holds the list of quotes from the selected category
     const randomQuote = list[Math.floor(Math.random() * list.length)];//picks a random quote from that list
 
@@ -111,8 +112,6 @@ app.get('/quotebook/quote/:category', (req, res) => {
   }else{
     return res.type('text').status(400).send({error: 'no category listed for ' + category});
   }
-
-
   /*
   let matchFound = false;
   let error ='no category listed for ' + category;
@@ -128,6 +127,19 @@ app.get('/quotebook/quote/:category', (req, res) => {
   }
     */
 });
+//Quotes 1.3: Create a POST endpoint at /quotebook/quote/new
+app.post('/quotebook/quote/new', (req, res) => {
+  const {category, quote, author} = req.body;
+  if (!category || !quote || !author || !categories.includes(category)) {//requires 3 parameters
+    return res.status(400).json({
+      error: "invalid or insufficient user input"
+    });
+  }
+  // Add the new quote object
+  categoryMap[category].push({quote: quote, author: author});
+  return res.type("text").send("Success!");
+});
+
 
 
 
